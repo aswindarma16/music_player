46dc16e00b174b6644d379ad676fe6448378bcbe
aswindarma16 <36687618+aswindarma16@users.noreply.github.com>
2022-09-04 17:43:41 +0700

    Delete lib directory

--- lib/models/music.dart
+++
@@ -1,5 +0,0 @@
-class Music {
-  final String artistName, trackName, previewUrl, imagePreview, albumName, trackId;
-
-  Music({required this.artistName, required this.trackName, required this.previewUrl, required this.imagePreview, required this.albumName, required this.trackId});
-}
